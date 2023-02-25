fb3fd83ef091f38f304fb621b47128581edefbc4
ravi-git-22 <ravikiran.thames@outlook.com>
2023-02-25 18:11:37 +0000

    commit-5

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_kubernetes_cluster" "acr_aks" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 3
     vm_size    = "Standard_D2_v2"
   }
 
